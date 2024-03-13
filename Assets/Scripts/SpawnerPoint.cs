eea3165909629dc542f637be80e06bfa10d66726
VitalikGrinya <127299895+VitalikGrinya@users.noreply.github.com>
2024-03-13 18:20:52 +0300

    Update and rename SpawnPoint.cs to SpawnerPoint.cs

---
+++ Assets/Scripts/SpawnerPoint.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class SpawnerPoint : MonoBehaviour
+{
+    [SerializeField] private Enemies _enemy;
+    [SerializeField] private float _degreesRotation;
+    public void Spawn()
+    {
+        var enemy = Instantiate(_enemy, transform.position, Quaternion.Euler(0, _degreesRotation, 0));
+    }
+}
