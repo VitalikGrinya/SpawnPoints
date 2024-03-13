1ad57329752fe2e65c86c2bf78d51020daa20aa5
VitalikGrinya <127299895+VitalikGrinya@users.noreply.github.com>
2024-03-13 18:21:23 +0300

    Create Spawn.cs

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    [SerializeField] private SpawnerPoint[] _spawnPoint;
+
+    private int minRandomValue = 0;
+    private int maxRandomValue = 3;
+
+    private float _spawnTime = 2;
+
+    private void Start()
+    {
+        var waitForSeconds = new WaitForSeconds(_spawnTime);
+
+        StartCoroutine(Spawning(waitForSeconds));
+    }
+
+    private IEnumerator Spawning(WaitForSeconds wait)
+    {
+        while (true)
+        {
+            ActivateSpawnPoint();
+
+            yield return wait;
+        }
+    }
+
+    private void ActivateSpawnPoint()
+    {
+        int currentSpawnPoint = Random.Range(minRandomValue, maxRandomValue);
+
+        SpawnerPoint spawnerPoint = _spawnPoint[currentSpawnPoint];
+        spawnerPoint.Spawn();
+    }
+}
