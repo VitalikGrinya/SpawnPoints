1a0d2b27fed9725799f2ae0535aa19dcae428635
VitalikGrinya <127299895+VitalikGrinya@users.noreply.github.com>
2024-06-25 10:00:52 +0300

    Update SpawnerPoint.cs

--- Assets/Scripts/SpawnerPoint.cs
+++ Assets/Scripts/SpawnerPoint.cs
@@ -4,6 +4,7 @@ public class SpawnerPoint : MonoBehaviour
 {
     [SerializeField] private Enemies _enemy;
     [SerializeField] private float _degreesRotation;
+    
     public void Spawn()
     {
         var enemy = Instantiate(_enemy, transform.position, Quaternion.Euler(0, _degreesRotation, 0));
